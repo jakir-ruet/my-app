23b26d5dd7c09e0017c2e82ff71042472b1d2732
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-12-15 16:54:01 +0600

    mapping with unique ID

--- src/container/mappingById/component/List.js
+++ src/container/mappingById/component/List.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const Lists = [
    {
+      id: uuidv4(),
       name: 'I am Jakir',
       address: 'Rajshahi'
    },
    {
+      id: uuidv4(),
       name: 'I am Jasim',
       address: 'Dhaka'
    },
    {
+      id: uuidv4(),
       name: 'I am Jakia',
       address: 'Chattogram'
    },
@@ -19,11 +23,16 @@ const ListView = () => {
    return (
       <div>
          <p>mapping by Id</p>
-         {Lists.map((List, index) => { 
+         {Lists.map((List) => {
+            const { id, name, address } = List;
             return (
-               <div key={index}>
-                  <p>{ List.name }</p>
-                  <p>{ List.address }</p>
+               // <div key={List.id}>
+               //    <p>{ List.name }</p>
+               //    <p>{ List.address }</p>
+               // </div>
+               <div key={id}>
+                  <p>{ name }</p>
+                  <p>{ address }</p>
                </div>
             )
          })}
