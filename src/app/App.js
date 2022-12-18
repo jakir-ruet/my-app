a3dc0a4311fd8edcbfeaa084d05ab4b7c88ef12e
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-12-18 13:42:05 +0600

    life cycle done

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,7 @@ import MyMapping from '../container/mapping/MyMapping';
 import MappingById from '../container/mappingById/MappingById';
 import NestedMap from '../container/nestedmap/NestedMap';
 import ClassComponent from '../container/classcomponent/ClassComponent';
-import StateLifeCycle from '../container/classcomponent/component/statelifecycle/component/Clock';
+import StateLifeCycle from '../container/classcomponent/statelifecycle/StateLifeCycle';
 
 function App() {
   return (
