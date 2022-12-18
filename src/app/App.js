3376fd3b7d4b8330b41909e7f77dd51665e19187
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-12-18 11:51:21 +0600

    the state life cycle going gon

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,7 @@ import MyMapping from '../container/mapping/MyMapping';
 import MappingById from '../container/mappingById/MappingById';
 import NestedMap from '../container/nestedmap/NestedMap';
 import ClassComponent from '../container/classcomponent/ClassComponent';
+import StateLifeCycle from '../container/classcomponent/component/statelifecycle/component/Clock';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <MyMapping />
       <MappingById />
       <NestedMap />
-      <ClassComponent/>
+      <ClassComponent />
+      <StateLifeCycle />
     </div>
   );
 }
---
+++ src/container/classcomponent/component/statelifecycle/StateLifeCycle.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Clock from "./component/Clock";
+
+function StateLifeCycle() {
+   return (
+      <div>
+         <Clock locale='en-US'/>
+      </div>
+   )
+}
+
+export default StateLifeCycle;
