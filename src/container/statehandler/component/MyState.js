e8121ac43449dbd66e87b649f070af0d07a1d506
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-12-18 19:32:00 +0600

    state understanding going on

--- src/container/statehandler/component/MyState.js
+++ src/container/statehandler/component/MyState.js
@@ -7,19 +7,27 @@ class MyState extends Component{
          count: 0
       }
    }
+   increment() {
+      this.state.count = this.state.count + 1
+      console.log(this.state.count)
+   }
+   decrement(){
+      this.state.count = this.state.count - 1
+      console.log(this.state.count)
+   }
    render() {
-      const { count } = this.state;
+      // const { count } = this.state;
       return (
          <div className="container">
             <div className="row text-center">
                <div className="col-md-6 offset-3">
                   <div className="d-flex flex-row bd-highlight">
                      <div className="p-2 bd-highlight">
-                        <button className="btn btn-primary">+</button>
+                        <button onClick={() => this.increment()} className="btn btn-primary">+</button>
                      </div>
-                     <div className="p-2 bd-highlight"><h2>{count}</h2></div>
+                     <div className="p-2 bd-highlight"><h2>{this.state.count}</h2></div>
                      <div className="p-2 bd-highlight">
-                        <button className="btn btn-primary">-</button>
+                        <button onClick={() => this.decrement()} className="btn btn-primary">-</button>
                      </div>
                   </div>
                </div>
