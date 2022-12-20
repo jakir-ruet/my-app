0e059bbf5d6fb001d7a7160ff6b68e76855bd647
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-12-20 16:53:46 +0600

    methods as a props done

--- src/container/Container.js
+++ src/container/Container.js
@@ -5,16 +5,18 @@ import ClassComponent from "./12-eventhandler/ClassComponent";
 import FuncComponent from "./12-eventhandler/FuncComponent";
 import Props from './9-props/MyProps';
 import EventBind from './13-eventbind/EventBind';
+import ParentComponent from './14-metsasprops/components/ParentComponent';
 
 function Container() {
    return (
       <div className="mt-2">
-         <Props />
-         <MyState />
-         <MySetState />
-         <FuncComponent />
-         <ClassComponent />
-         <EventBind/>
+         {/* <Props /> */}
+         {/* <MyState /> */}
+         {/* <MySetState /> */}
+         {/* <FuncComponent /> */}
+         {/* <ClassComponent /> */}
+         {/* <EventBind/> */}
+         <ParentComponent />
       </div>
    )
 }
