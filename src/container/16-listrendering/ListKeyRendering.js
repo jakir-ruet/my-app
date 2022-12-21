f6f68645befeb2720d53ad475346d48d9a13e789
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-12-21 15:52:35 +0600

    list & key done

--- src/container/16-listrendering/ListKeyRendering.js
+++ src/container/16-listrendering/ListKeyRendering.js
@@ -1,34 +1,21 @@
 import React from "react";
-import Staff from "./component/Staff";
 
 function ListKeyRendering() {
    const staffsName = ['Jakir', 'Jasim', 'Jakia', 'Jabir']
-   const staffs = [
-      {
-         id: 1,
-         name: 'Jakir',
-         age: 35,
-         skill: 'ReactJS'
-      },
-      {
-         id: 2,
-         name: 'Jasim',
-         age: 30,
-         skill: 'NodeJS'
-      },
-      {
-         id: 1,
-         name: 'Jakia',
-         age: 25,
-         skill: 'NextJS'
-      }
-   ]
-   // const staffList = staffs.map(staff => < Staff key={staff.id} staff={staff} />)
-   const staffName = staffsName.map((a, index) => <p key={index}>{index}{a}</p>)
+   const staffName = staffsName.map((name, index) => <p key={index}>{name}</p>)
    return (
       <div className="card m-2">
          <div className="card-body">
-            {staffName}
+            <div className="card m-2">
+               <div className="card-body">
+                  {staffsName}
+               </div>
+            </div>
+            <div className="card m-2">
+               <div className="card-body">
+                  {staffName}
+               </div>
+            </div>
          </div>
       </div>
    )
